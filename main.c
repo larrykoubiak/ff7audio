d995029fe98db1697bb35a8c7a553f3711428981
unknown <u401065@L211714.post.bpgnet.net>
2014-06-03 13:31:28 +0200

    first commit

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int main(int argc, char **argv) {
 	if(argc == 2) {
 		filename = argv[1];
 	} else if(argc ==3 && strcmp(argv[1],"-reverb") == 0) {
+		reverb_len = REVERB_BUF_LEN;
 		filename = argv[2];
 	} else {
 		fprintf(stderr,"Usage: %s [-reverb] filename.\n",argv[0]);	
